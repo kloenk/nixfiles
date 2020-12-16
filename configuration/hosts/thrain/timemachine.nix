3ce464099df292912667c34073a10f36a8fd75a7
Finn Behrens <me@kloenk.de>
2020-12-16 12:28:02 +0100

    uac netatalk

--- configuration/hosts/thrain/timemachine.nix
+++ configuration/hosts/thrain/timemachine.nix
@@ -4,16 +4,24 @@
   networking.firewall.allowedTCPPorts = [
     548 # netatalk
   ];
+  networking.firewall.allowedUDPPorts = [
+    548 # netatalk
+  ];
 
   services = {
     netatalk = {
       enable = true;
+
+      extraConfig = lib.mkBefore (''
+        hosts allow = 192.168.178.0/24 6.0.2.0/24
+      '');
 
       volumes = {
         "kloenk-time-machine" = {
           "time machine" = "yes";
           path = "/persists/data/timemachine";
           "valid users" = "kloenk";
+          #"rwlist" = "nobody";
         };
       };
     };
