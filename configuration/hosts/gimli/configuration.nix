1b62d637013b56331a20c54c95de28d0a97db725
Finn Behrens <me@kloenk.de>
2020-12-31 20:59:03 +0100

    reinit gimli

--- configuration/hosts/gimli/configuration.nix
+++ configuration/hosts/gimli/configuration.nix
@@ -16,57 +16,47 @@
   # vm connection
   services.qemuGuest.enable = true;
 
-  boot.supportedFilesystems = [ "zfs" "vfat" ];
+  boot.supportedFilesystems = [ "zfs" "vfat" "xfs" ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.grub.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.initrd.luks.reusePassphrases = true;
+  boot.initrd.luks.devices.cryptRoot.device =
+    "/dev/disk/by-uuid/9a1b13f9-d9d3-4df4-8a97-7f2d652e0c4e";
+  boot.initrd.luks.devices.cryptSwap.device =
+    "/dev/disk/by-uuid/3b7e77ec-1627-4a6b-b3fb-242bf1b1ecbc";
 
   boot.initrd.network.enable = true;
   boot.initrd.availableKernelModules = [ "virtio-pci" ];
   boot.initrd.network.ssh = {
     enable = true;
-    hostKeys = [ "/var/src/secrets/initrd/ed25519_host_key" ];
+    #hostKeys = [ "/var/src/secrets/initrd/ed25519_host_key" ];
   };
-  boot.initrd.network.postCommands = ''
-    echo "zfs load-key -a; killall zfs" >> /root/.profile
-  '';
+  /*boot.initrd.network.postCommands = ''
+  '';*/
 
   # setup network
-  boot.initrd.preLVMCommands = lib.mkBefore (''
+  /*boot.initrd.preLVMCommands = lib.mkBefore (''
     ip li set ens18 up
     ip addr add 195.39.221.51/32 dev ens18
     ip route add default via 195.39.221.1 onlink dev ens18 && hasNetwork=1
-  '');
+  '');*/
 
   # delete files in /
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
+  /*boot.initrd.postDeviceCommands = lib.mkAfter ''
     zfs rollback -r spool/local/root@blank
-  '';
+  '';*/
 
   networking.hostName = "gimli";
   networking.hostId = "8425e349";
   networking.dhcpcd.enable = false;
   networking.useDHCP = false;
-  networking.interfaces.ens18.ipv4.addresses = [{
-    address = "195.39.221.51";
-    prefixLength = 32;
-  }];
-  networking.interfaces.ens18.ipv6.addresses = [{
-    address = "2a0f:4ac4:42:f144:fa::51";
-    prefixLength = 64;
-  }];
 
-  systemd.network.networks."40-ens18".routes = [
-    {
-      routeConfig.Gateway = "195.39.221.1";
-      routeConfig.GatewayOnLink = true;
-    }
-    {
-      routeConfig.Gateway = "fe80::5881:4bff:fe7b:615b";
-      #routeConfig.GatewayOnLink = true;
-    }
-  ];
+  systemd.network.networks."40-ens18" = {
+    name = "enp1s0";
+    DHCP = "yes";
+  };
 
   system.autoUpgrade.enable = true;
   nix.gc.automatic = true;
@@ -76,6 +66,11 @@
     xz.bin
     gzip
     config.nix.package.out
+  ];
+
+  # acme files
+  systemd.tmpfiles.rules = [
+    "L /var/lib/acme - - - - /persist/acme"
   ];
 
   system.stateVersion = "21.03";
--- configuration/hosts/gimli/hardware-configuration.nix
+++ configuration/hosts/gimli/hardware-configuration.nix
@@ -8,46 +8,33 @@
     [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "spool/local/root";
-      fsType = "zfs";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7977-CFB7";
-      fsType = "vfat";
+    { device = "/dev/disk/by-uuid/0a556e8a-e849-4d3e-b701-ecee617c92a9";
+      fsType = "xfs";
     };
 
   fileSystems."/nix" =
-    { device = "spool/local/nix";
-      fsType = "zfs";
+    { device = "/dev/disk/by-uuid/e25fb804-736c-4ff1-819e-d70738d29297";
+      fsType = "xfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/9EA4-E311";
+      fsType = "vfat";
     };
 
   fileSystems."/persist" =
-    { device = "spool/safe/persist";
-      fsType = "zfs";
+    { device = "/dev/disk/by-uuid/506b3452-d9d5-466a-bbbb-836f72f05961";
+      fsType = "xfs";
     };
 
-  fileSystems."/persist/postgres" =
-    { device = "spool/safe/postgresql";
-      fsType = "zfs";
-    };
-
-  fileSystems."/var/log" =
-    { device = "spool/local/log";
-      fsType = "zfs";
-    };
-
-  fileSystems."/persist/matrix" =
-    { device = "hpool/safe/matrix";
-      fsType = "zfs";
-    };
-
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/d2d2f5d2-22db-435a-9b5a-872a236f964e"; }
+    ];
 
 }
