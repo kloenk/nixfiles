8a10d86451524b7716a1e08ae49471a1768b357a
Finn Behrens <me@kloenk.de>
2021-01-21 21:38:28 +0100

    move maps to data

---
+++ configuration/hosts/iluvatar/fediventure.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  services.workadventure.instances."play.kloenk.dev" = {
+    nginx.domain = "play.kloenk.dev";
+    /*backend.package = wapkgs.back;
+    puscher.package = wapkgs.pusher;
+    frontend.package = wapkgs.front;
+    maps.package = wapkgs.maps;*/
+  };
+
+  services.nginx.virtualHosts."play.kloenk.dev" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/maps/" = {
+      alias = lib.mkForce "/persist/data/wa-maps/workadventuremaps";
+    };
+  };
+}
