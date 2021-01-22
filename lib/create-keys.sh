2e60f4b3e31b4d18bbf25c2db6623ad5170133d3
Finn Behrens <me@kloenk.de>
2021-01-22 23:18:59 +0100

    reencrypt secrets

--- lib/create-keys.sh
+++ lib/create-keys.sh
@@ -8,7 +8,7 @@ for i in $(cat ./lib/hosts)
 do
     hostname=$(ssh $i "hostname")
     echo $hostname
-    #ssh $i "sudo rm -rf /root/.gnupg/*"
+    ssh $i "sudo sh -c 'rm -rf /root/.gnupg/*'"
     cat lib/keygen | sed "s/NAME/${hostname}/" | ssh -o RequestTTY=yes $i "sudo gpg --generate-key --pinentry-mode loopback --batch /dev/stdin"
     cp ./secrets/.gpg-id ./secrets/$hostname/.gpg-id
     ssh $i "sudo -u root gpg --fingerprint --with-colons | grep '^fpr' | head -n1 | cut -d: -f10" >> ./secrets/$hostname/.gpg-id
