a01be0468d3568d85d747e78c38bd3c6c1b419ae
Finn Behrens <me@kloenk.de>
2021-02-16 11:19:15 +0100

    update cache control for event

--- configuration/hosts/event/nginx.nix
+++ configuration/hosts/event/nginx.nix
@@ -14,6 +14,19 @@ in {
 
   nixpkgs.overlays = [ inputs.event_start.overlay ];
 
+  services.nginx.commonHttpConfig = ''
+    map $sent_http_content_type $cacheable_types {
+      "text/html"               "public; max-age=600; must-revalidate";
+      "text/plain"              "public; max-age=3600; must-revalidate";
+      "application/json"        "public; max-age=60; must-revalidate";
+      "text/css"                "public; max-age=15778800; immutable";
+      "application/javascript"  "public; max-age=15778800; immutable";
+      "font/woff2"              "public; max-age=15778800; immutable";
+      "application/xml"         "public; max-age=3600; must-revalidate";
+      default                   "public; max-age=1209600";
+    }
+  '';
+
   services.nginx.virtualHosts = {
     "event.unterbachersee.de" = {
       locations."/robots.txt".return = "200 \"User-agent: *\\nDisallow: /\\n\"";
