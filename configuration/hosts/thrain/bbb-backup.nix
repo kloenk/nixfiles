b5e4f1ccb0a893febc0015a076083213323d9544
Finn Behrens <me@kloenk.de>
2021-03-14 12:13:02 +0100

    add rsync

--- configuration/hosts/thrain/bbb-backup.nix
+++ configuration/hosts/thrain/bbb-backup.nix
@@ -7,14 +7,17 @@ let
       after = [ "network.target" ];
       script = ''
         ${pkgs.rsync}/bin/rsync -a -e "ssh -o UserKnownHostFile=/etc/bbb_hostkey -o IdentityFile=$CREDENTIALS_DIRECTORY/bbb_creds" \
-          ssh://root@stream.unterbachersee.de:${dir}/ /var/lib/bbb_backup/${targetDir}/
+          root@stream.unterbachersee.de:${dir}/ /var/lib/bbb_backup/${targetDir}/
       '';
 
       serviceConfig = {
         LoadCredential = "bbb_creds:${config.petabyte.secrets."bbb_creds".path}";
+        StateDirectory = "bbb_backup/${targetDir}";
         BindPaths = "/persist/bbb_backupvideos/${targetDir}:/var/lib/bbb_backup/${targetDir}";
         ReadWritePaths = "/persist/bbb_backupvideos/${targetDir}";
-        DynamicUser = true;
+        DynamicUser = false;
+        User = "bbb_backup";
+        Group = "bbb_backup";
 
         # sandboxing
         NoNewPrivileges = true;
@@ -41,12 +44,19 @@ in
 {
   petabyte.secrets."nas_creds".owner = "root";
   petabyte.secrets."bbb_creds".owner = "root";
-  environment.etc."bbb_hostkey".text = "stream.unterbachersee.de,2a01:4f8:192:430b::2 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLqJOQVdy5FlBV77JPVHLMKd5hg1LGW4alDj52Aky3qj7hY95gwzFHnV8UoZqQmt+t4y2i/fT1vWdwDBfQY7zxM=";
+  environment.etc."bbb_hostkey".text = "stream.unterbachersee.de,144.76.103.12,2a01:4f8:192:430b::2 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLqJOQVdy5FlBV77JPVHLMKd5hg1LGW4alDj52Aky3qj7hY95gwzFHnV8UoZqQmt+t4y2i/fT1vWdwDBfQY7zxM=";
+
+  users.users.bbb_backup = {
+    isSystemUser = true;
+    createHome = false;
+    group = "bbb_backup";
+  };
+  users.group.bbb_backup = {};
 
   fileSystems."/persist/bbb_backupvideos" = {
     device = "//192.168.178.68/bbb_backupvideos";
     fsType = "cifs";
-    options = [ "credentials=${config.petabyte.secrets."nas_creds".path}" ];
+    options = [ "credentials=${config.petabyte.secrets."nas_creds".path}" "uid=bbb_backup" "gid=bbb_backup" ];
   };
 
 } // backupDir { dir = "/var/bigbluebutton/published/presentation"; targetDir = "published"; }
