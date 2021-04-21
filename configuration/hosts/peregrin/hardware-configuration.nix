e3e75eb6d5e71b2f8eb42dad015408f815705951
Finn Behrens <me@kloenk.de>
2021-04-21 13:24:10 +0200

    update hardware-config

--- configuration/hosts/peregrin/hardware-configuration.nix
+++ configuration/hosts/peregrin/hardware-configuration.nix
@@ -7,18 +7,28 @@
   imports = [ ];
 
   boot.initrd.availableKernelModules = [ "usbhid" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6ffad845-de3a-4f33-8082-253048884a93";
+    { device = "/dev/disk/by-uuid/b927fa3e-682b-4115-b63b-cdef5fb28039";
       fsType = "xfs";
     };
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/B39C-C99E";
       fsType = "vfat";
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/ee5b0e76-3b3c-46c4-b6ac-c78b75c1d8a4";
+      fsType = "xfs";
+    };
+
+  fileSystems."/persist" =
+    { device = "/dev/disk/by-uuid/bfb6918c-27bc-4ed7-8ff0-2e89f5dd37c7";
+      fsType = "xfs";
     };
 
   swapDevices = [ ];
