ea2b8fa6e897b2bccf366948307b84f7f1c929d7
Finn Behrens <me@kloenk.de>
2021-05-21 11:05:03 +0200

    update moodle

--- configuration/hosts/moodle-usee/moodle.nix
+++ configuration/hosts/moodle-usee/moodle.nix
@@ -1,71 +1,10 @@
 { pkgs, lib, config, ... }:
 
-let
-  bbb = pkgs.moodle-utils.buildMoodlePlugin {
-    name = "bigbluebuttonbn";
-    src = pkgs.fetchzip {
-      name = "bbb-moodle-plugin";
-      url =
-        "https://moodle.org/plugins/download.php/23293/mod_bigbluebuttonbn_moodle310_2019042011.zip";
-      sha256 =
-        "12aclcjm3wq8lz7ks3nk1pb0is2a2wljy7db6qjc2v4hjnybhrlq";
-      extraPostFetch = ''
-        echo fix perm: $out
-        chmod a-w -R $out
-      '';
-    };
-    pluginType = "mod";
-  };
-  tiles = pkgs.moodle-utils.buildMoodlePlugin {
-    name = "tiles";
-    src = pkgs.fetchzip {
-      name = "tiles";
-      url =
-        "https://moodle.org/plugins/download.php/23359/format_tiles_moodle310_2020080613.zip";
-      sha256 =
-        "1vya5j7nhyb1xmkp3ia26zb7k2w0z8axpqr2scgbd2kmb8lqgccq";
-      extraPostFetch = ''
-        echo fix perm: $out
-        chmod a-w -R $out
-      '';
-    };
-    pluginType = "course";
-  };
-  sharing_cart = pkgs.moodle-utils.buildMoodlePlugin {
-    name = "sharing_cart";
-    src = pkgs.fetchzip {
-      name = "sharing_cart";
-      url =
-        "https://moodle.org/plugins/download.php/23686/block_sharing_cart_moodle310_2021031300.zip";
-      sha256 =
-        "1g1hvjllkxh5q7vzh0jmf257id51dc88laf6l2mqvwwz5wq4y303";
-      extraPostFetch = ''
-        echo fix perm: $out
-        chmod a-w -R $out
-      '';
-    };
-    pluginType = "block";
-  };
-  scheduler = pkgs.moodle-utils.buildMoodlePlugin {
-    name = "scheduler";
-    src = pkgs.fetchzip {
-      name = "scheduler";
-      url =
-        "https://moodle.org/plugins/download.php/20738/mod_scheduler_moodle39_2019120200.zip";
-      sha256 =
-        "0n5qbqcb3j631j6q7fw0anffp7hgc2c7mg8z3i5z6fpi1pc20sjg";
-      extraPostFetch = ''
-        echo fix perm: $out
-        chmod a-w -R $out
-      '';
-    };
-    pluginType = "mod";
-  };
-in {
+{
   services.moodle = {
     enable = true;
     initialPassword = "foobar123";
-    package = pkgs.moodle.override { plugins = [ bbb tiles sharing_cart scheduler ]; };
+    package = pkgs.moodle.override { plugins = with pkgs.moodlePackages; [ bbb tiles sharing_cart scheduler ]; };
     virtualHost = {
       adminAddr = "holger.behrens@unterbachersee.de";
       enableACME = true;
