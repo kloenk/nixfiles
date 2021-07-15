11c9376199ffa73c58e9ff2effba472a4facd0ca
Finn Behrens <me@kloenk.de>
2021-07-15 17:09:04 +0200

    fabric sandbox

--- configuration/hosts/manwe/fabric.nix
+++ configuration/hosts/manwe/fabric.nix
@@ -2,25 +2,39 @@
 
 {
   systemd.services.fabric = {
-    description = "Minecraft ftb server";
+    description = "Minecraft fabric server";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     requires = [ "fabric.socket" ];
     serviceConfig = {
-      BindPaths = "/persist/data/terra-indrev:/var/lib/terra-indrev";
+      BindPaths = "/persist/data/terra-indrev:/var/lib/private/fabric";
       ReadWritePaths = "/persist/data/terra-indrev";
       Restart = "always";
       DynamicUser = true;
-      StateDirectory = "terra-indrev";
+      StateDirectory = "fabric";
       StandardInput = "socket";
       StandardOutput = "journal";
+      WorkingDirectory="/var/lib/fabric";
+
+      ExecStart = "${pkgs.jdk16_headless}/bin/java -Xms512M -Xmx4G -jar fabric-server-launch.jar --nogui";
+
+      # Sandboxing
+      NoNewPrivileges = true;
+      PrivateTmp = true;
+      PrivateDevices = true;
+      ProtectSystem = "strict";
+      ProtectHome = true;
+      ProtectControlGroups = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" "AF_NETLINK" ];
+      RestrictRealtime = true;
+      RestrictNamespaces = true;
+      MemoryDenyWriteExecute = true;
     };
-    script = ''
-      pwd
-      cd /var/lib/terra-indrev
-      ls
+    /*script = ''
       ${pkgs.jdk16_headless}/bin/java -Xms512M -Xmx4G -jar fabric-server-launch.jar nogui
-    '';
+    '';*/
   };
   systemd.sockets.fabric = {
     description = "Minecraft stdin fifo file";
