52f0bbe177b37813b0e1093eca5ad885f4491f7d
Finn Behrens <me@kloenk.dev>
2021-08-18 10:35:12 +0200

    enable aarch64-linux

--- flake.nix
+++ flake.nix
@@ -119,7 +119,7 @@
         rtmp-auth.overlay
       ];
 
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
 
