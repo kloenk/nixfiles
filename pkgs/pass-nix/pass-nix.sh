3dffb3cd510945254d4b0fe16f3a959789247a82
Finn Behrens <me@kloenk.dev>
2021-08-23 14:53:58 +0200

    fix durin ssh key

--- pkgs/pass-nix/pass-nix.sh
+++ pkgs/pass-nix/pass-nix.sh
@@ -1,5 +1,5 @@
 #!@shell@
-export PATH=@path@:/run/wrappers/bin/
+export PATH=@path@:/run/wrappers/bin/:/usr/bin/:/sbin:/usr/sbin
 for key in secrets/.public-keys/*
 do
 	gpg --import $key 2>/dev/null
