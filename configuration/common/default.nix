6b57670b3b14ba6cbbb503ec52a127c2d72fa68c
Finn Behrens <me@kloenk.dev>
2021-08-23 19:47:12 +0200

    durin: add dedicated rbuild.service file

--- configuration/common/default.nix
+++ configuration/common/default.nix
@@ -40,7 +40,7 @@
   networking.useDHCP = lib.mkDefault false;
   networking.interfaces.lo = lib.mkDefault {
     ipv4.addresses = [
-      { address = "127.0.0.1"; prefixLength = 32; }
+      { address = "127.0.0.1"; prefixLength = 8; }
       { address = "127.0.0.53"; prefixLength = 32; }
     ];
   };
--- configuration/hosts/durin/containers/rbuild.nix
+++ configuration/hosts/durin/containers/rbuild.nix
@@ -10,6 +10,10 @@ in {
     fsType = "none";
     options = [ "bind" ];
   };
+  fileSystems."/persist/data/rbuild/cache" = {
+    device = "/dev/disk/by-uuid/4a78ab01-9338-4062-9f9b-c56db755efb3";
+    fsType = "xfs";
+  };
 
   networking.firewall.allowedTCPPorts = [
     ports.ssh # ssh
@@ -19,7 +23,50 @@ in {
     description = "SSH socket for durin-rbuild systemd container";
     wantedBy = [ "sockets.target" ];
     listenStreams = [ (toString ports.ssh) ];
-    socketConfig.Service = "systemd-nspawn@rbuild";
+  };
+
+  systemd.services.rbuild = {
+    description = "Airlink build systemd container";
+    wantedBy = [ "machines.target" ];
+    wants = [ "modprobe@tun.service" "modprobe@loop.service" "modprobe@dm-mod.service" ];
+    partOf = [ "machines.target" ];
+    before = [ "machines.target" ];
+    after = [
+      "network.target"
+      "systemd-resolved.service"
+      "modprobe@tun.service"
+      "modprobe@loop.service"
+      "modprobe@dm-mod.service"
+    ];
+
+    unitConfig.RequiresMountsFor = [ "/var/lib/machines/rbuild" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.systemd}/bin/systemd-nspawn --quiet --keep-unit --boot --network-veth -U --settings=override --machine=rbuild";
+      KillMode = "mixed";
+      Type = "notify";
+      RestartForceExitStatus = "133";
+      SuccessExitStatus = "133";
+      Slice = "machine.slice";
+      Delegate = "yes";
+      TasksMax = "16384";
+      WatchdogSec = "3min";
+
+      # Enforce a strict device policy, similar to the one nspawn configures when it
+      # allocates its own scope unit. Make sure to keep these policies in sync if you
+      # change them!
+      DevicePolicy = "closed";
+      DeviceAllow = [
+        "/dev/net/tun rwm"
+        "char-pts rw"
+        "/dev/loop-control rw"
+        "block-loop rw"
+        "block-blkext rw"
+        "/dev/kvm rm"
+        "/dev/mapper/control rw"
+        "block-device-mapper rw"
+      ];
+    };
   };
 
   systemd.nspawn.rbuild = {
@@ -30,9 +77,10 @@ in {
     };
     filesConfig = {
       Bind = [
-        #"/persist/data/rbuild/cache:/home/fin/cache"
-        #"/persist/data/rbuild/out:/home/fin/packages"
-        #"/persist/data/airlink/proj:/home/fin/airlink"
+        "/persist/data/rbuild/cache:/home/fin/cache"
+        "/persist/data/rbuild/out:/home/fin/packages"
+        "/persist/data/airlink/proj:/home/fin/airlink"
+        "/dev/kvm"
       ];
     };
     execConfig = {
