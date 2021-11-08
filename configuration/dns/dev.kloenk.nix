7652a8a47fcb5fde22e1718e30d7ab5039c3ba76
Finn Behrens <me@kloenk.dev>
2021-11-08 14:48:03 +0100

    add lab for gitlab

--- configuration/dns/dev.kloenk.nix
+++ configuration/dns/dev.kloenk.nix
@@ -35,7 +35,7 @@ let
     SOA = ((ttl 600) {
       nameServer = "ns1.kloenk.dev.";
       adminEmail = "hostmaster.kloenk.dev."; # TODO: change mail
-      serial = 2021010125;
+      serial = 2021010126;
       refresh = 3600;
       expire = 604800;
       minimum = 600;
@@ -93,6 +93,7 @@ let
       turn = gimli;
 
       pleroma = manwe;
+      lab = manwe;
 
       grafana = manwe;
       prometheus = manwe;
