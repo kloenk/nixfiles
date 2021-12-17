bd0b165f5b251cfa0292ca146b9f45686733ad34
Finn Behrens <me@kloenk.dev>
2021-12-17 16:27:59 +0100

    state foo

--- configuration/hosts/thrain/k3s.nix
+++ configuration/hosts/thrain/k3s.nix
@@ -1,6 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
+
+  fileSystems."/var/lib/kubelet" = {
+    device = "/persist/data/kubelet";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/rancher" = {
+    device = "/persist/data/rancher";
+    options = [ "bind" ];
+  };
+
   networking.firewall.allowedTCPPorts = [ 6443 ];
 
   services.k3s = {
