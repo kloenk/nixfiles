7b800d97d468a56fee8b66433ad37e8fbbee4af6
Finn Behrens <me@kloenk.dev>
2021-12-17 17:37:37 +0100

    🤔

--- configuration/hosts/thrain/k3s.nix
+++ configuration/hosts/thrain/k3s.nix
@@ -1,6 +1,27 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  flannel = builtins.toJSON {
+    name = "cbr0";
+    cniVersion = "0.3.1";
+    plugins = [
+      {
+        type = "flannel";
+        delegate = {
+          hairpinMode = true;
+          forceAddress = true;
+          isDefaultGateway = true;
+        };
+      }
+      {
+        type = "portmap";
+        capabilities = {
+          portMappings = true;
+        };
+      }
+    ];
+  };
+in {
 
   fileSystems."/var/lib/kubelet" = {
     device = "/persist/data/kubelet";
@@ -16,6 +37,21 @@
   services.k3s = {
     enable = true;
     role = "server";
+    docker = lib.mkForce false;
+  };
+  virtualisation.containerd = {
+    enable = true;
+    settings = {
+      version = 2;
+      plugins."io.containerd.grpc.v1.cri" = {
+        cni.conf_dir = "${pkgs.writeTextDir "net.d/10-flannel.conflist" flannel}/net.d";
+        # FIXME: upstream
+        cni.bin_dir = "${pkgs.runCommand "cni-bin-dir" {} ''
+          mkdir -p $out
+          ln -sf ${pkgs.cni-plugins}/bin/* ${pkgs.cni-plugin-flannel}/bin/* $out
+        ''}";
+      };
+    };
   };
 
   environment.systemPackages = [ config.services.k3s.package ];
