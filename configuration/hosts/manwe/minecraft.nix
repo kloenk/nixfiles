cde4b634bfbc79074a421b3292bd5bee96455bb0
Finn Behrens <me@kloenk.dev>
2022-01-07 14:46:56 +0100

    change jdk version

--- configuration/hosts/manwe/minecraft.nix
+++ configuration/hosts/manwe/minecraft.nix
@@ -10,7 +10,7 @@
   systemd.services.minecraft = {
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.jdk}/bin/java -Dlog4j2.formatMsgNoLookups=true -jar fabric-server-launch.jar nogui";
+      ExecStart = "${pkgs.jdk17_headless}/bin/java -Dlog4j2.formatMsgNoLookups=true -jar fabric-server-launch.jar nogui";
       RuntimeDirectory = "minecraft";
       RootDirectory = "/run/minecraft";
       StateDirectory = "minecraft";
