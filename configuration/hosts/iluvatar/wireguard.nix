1f5a809f1a02498fac724355c7f5f2055b815f35
Finn Behrens <me@kloenk.dev>
2022-01-22 21:02:05 +0100

    add wg2

--- configuration/hosts/iluvatar/wireguard.nix
+++ configuration/hosts/iluvatar/wireguard.nix
@@ -3,7 +3,7 @@
 {
   networking.firewall.allowedUDPPorts = [
     51820 # wg0
-    51830 # yougen
+    51830 # wg2
   ];
 
   # NATING
@@ -161,26 +161,26 @@
     "172.16.16.3" = [ "core.josefstrasse.yougen.de" ];
   };
 
-  systemd.network.netdevs."30-yougen" = {
+  systemd.network.netdevs."30-wg2" = {
     netdevConfig = {
       Kind = "wireguard";
-      Name = "yougen";
+      Name = "wg2";
     };
     wireguardConfig = {
       FirewallMark = 51820;
       ListenPort = 51830;
-      PrivateKeyFile = config.petabyte.secrets."yougen.key".path;
+      PrivateKeyFile = config.petabyte.secrets."wg2.key".path;
     };
     wireguardPeers = [{
       wireguardPeerConfig = {
         AllowedIPs = [ "172.16.16.3/32" ];
-        PublicKey = "UDdUoBRXy+3skbUuh7gLNmHnnbtJPncbCPPeZNX/rBU=";
+        PublicKey = "1uKmuo0q9GYOAPmYi3e20AHPech6XlpRTyrdsqGvkgs=";
         PersistentKeepalive = 21;
       };
     }];
   };
-  systemd.network.networks."30-yougen" = {
-    name = "yougen";
+  systemd.network.networks."30-wg2" = {
+    name = "wg2";
     linkConfig.RequiredForOnline = "no";
     addresses = [{ addressConfig.Address = "172.16.16.1/24"; }];
     routes = [{ routeConfig.Destination = "172.16.16.0/24"; }];
@@ -188,5 +188,5 @@
 
   users.users.systemd-network.extraGroups = [ "keys" ];
   petabyte.secrets."wg0.key".owner = "systemd-network";
-  petabyte.secrets."yougen.key".owner = "systemd-network";
+  petabyte.secrets."wg2.key".owner = "systemd-network";
 }
