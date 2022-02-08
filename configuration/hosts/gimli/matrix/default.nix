5d7ee3c14291212b32da2a6f08bac93a8c9aed9a
Finn Behrens <me@kloenk.dev>
2022-02-08 14:26:45 +0100

    add registration secret to matrix

--- configuration/hosts/gimli/matrix/default.nix
+++ configuration/hosts/gimli/matrix/default.nix
@@ -88,6 +88,9 @@
     extraConfig = ''
       max_upload_size: "100M"
     '';
+    extraConfigFiles = [
+      config.sops.secrets."matrix/config".path
+    ];
 
     url_preview_enabled            = true;
     url_preview_ip_range_blacklist = ["127.0.0.0/8" "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16" "100.64.0.0/10" "169.254.0.0/16" "::1/128" "fe80::/64" "fc00::/7"];
@@ -95,5 +98,6 @@
   
   sops.secrets."matrix/exmpp".owner = "matrix-synapse";
   sops.secrets."matrix/exmpp2".owner = "matrix-synapse";
+  sops.secrets."matrix/config".owner = "matrix-synapse";
   systemd.services.matrix-synapse.serviceConfig.SupplementaryGroups = [ config.users.groups.keys.name ];
 }
