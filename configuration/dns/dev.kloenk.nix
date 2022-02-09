5b54fd95886e5e34d61c13b0d3ebf54cf60c4823
Finn Behrens <me@kloenk.dev>
2022-02-09 16:15:55 +0100

    add unifi.kloenk.dev

--- configuration/dns/dev.kloenk.nix
+++ configuration/dns/dev.kloenk.nix
@@ -35,7 +35,7 @@ let
     SOA = ((ttl 600) {
       nameServer = "ns1.kloenk.dev.";
       adminEmail = "hostmaster.kloenk.dev."; # TODO: change mail
-      serial = 2021010132;
+      serial = 2021010133;
       refresh = 3600;
       expire = 604800;
       minimum = 600;
@@ -101,6 +101,7 @@ let
       influx = manwe;
       alertmanager = manwe;
       m-sandbox = manwe;
+      unifi = manwe;
 
       matrix = gimli // {
         subdomains.api = gimli;
