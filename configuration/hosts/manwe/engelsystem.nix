7337bfd2a747e2d550f64d4ca1c13a4e5b152a3d
Finn Behrens <me@kloenk.dev>
2022-04-08 15:17:29 +0200

    add email to engelsystem

--- configuration/hosts/manwe/engelsystem.nix
+++ configuration/hosts/manwe/engelsystem.nix
@@ -7,14 +7,14 @@
     config = {
       maintenance = false;
       database.username = "engelsystem";
-      /*email = {
+      email = {
         driver = "smtp";
         from.address = "noreply-punkte@kloenk.de";
-        from.name = "Abi 2021 Punktesystem";
+        from.name = "Abi Punktesystem";
         encryption = "tls";
         username = "noreply-punkte@kloenk.de";
-        password._secret = config.petabyte.secrets."es_mail_password".path;
-      };*/
+        password._secret = config.sops.secrets."es_mail_password".path;
+      };
       autoarrive = true;
       min_password_length = 6;
       enable_dect = false;
@@ -24,6 +24,8 @@
       default_locale = "de_DE";
     };
   };
+
+  sops.secrets."es_mail_password".owner = "engelsystem";
 
   services.nginx.virtualHosts."engel.rssr.kloenk.dev" = {
     enableACME = true;
