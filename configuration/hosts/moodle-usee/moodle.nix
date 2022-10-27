2627f23050801e60d7680e9e414bde9ece6ae434
Finn Behrens <me@kloenk.dev>
2022-10-27 15:26:42 +0200

    add lightboxgallery to usee moodle

--- configuration/hosts/moodle-usee/moodle.nix
+++ configuration/hosts/moodle-usee/moodle.nix
@@ -4,30 +4,36 @@
   services.moodle = {
     enable = true;
     initialPassword = "foobar123";
-    package = pkgs.moodle.override { plugins = with pkgs.moodlePackages; [ /* bigbluebuttonbn */ tiles sharing_cart scheduler ]; };
+    package = pkgs.moodle.override {
+      plugins = with pkgs.moodlePackages; [ # bigbluebuttonbn
+        tiles
+        sharing_cart
+        scheduler
+        lightboxgallery
+      ];
+    };
     virtualHost = {
       adminAddr = "holger.behrens@unterbachersee.de";
       enableACME = true;
       forceSSL = true;
       hostName = "segelschule.unterbachersee.de";
     };
-    database = {
-      type = "pgsql";
-    };
+    database = { type = "pgsql"; };
     extraConfig = ''
       $CFG->xsendfile = 'X-Accel-Redirect';
       $CFG->xsendfilealiases = array(
         '/dataroot/' => $CFG->dataroot
       );
     '';
-    /*poolConfig = {
-      "pm" = "dynamic";
-      "pm.max_children" = 32;
-      "pm.max_requests" = 500;
-      "pm.max_spare_servers" = 4;
-      "pm.min_spare_servers" = 2;
-      "pm.start_servers" = 2;
-    };*/
+    /* poolConfig = {
+         "pm" = "dynamic";
+         "pm.max_children" = 32;
+         "pm.max_requests" = 500;
+         "pm.max_spare_servers" = 4;
+         "pm.min_spare_servers" = 2;
+         "pm.start_servers" = 2;
+       };
+    */
   };
 
   services.nginx.virtualHosts."segelschule.unterbachersee.de" = {
@@ -45,13 +51,13 @@
     root = "${config.services.moodle.package}/share/moodle";
     locations."~ [^/]\\.php(/|$)" = {
       root = "${config.services.moodle.package}/share/moodle";
-         #fastcgi_split_path_info  ^(.+\.php)(.*)$;
+      #fastcgi_split_path_info  ^(.+\.php)(.*)$;
       extraConfig = ''
-         fastcgi_index            index.php;
-         fastcgi_param   SCRIPT_FILENAME $document_root$fastcgi_script_name;
-         fastcgi_pass             unix:${config.services.phpfpm.pools.moodle.socket};
-         include ${config.services.nginx.package}/conf/fastcgi_params;
-         include ${config.services.nginx.package}/conf/fastcgi.conf;
+        fastcgi_index            index.php;
+        fastcgi_param   SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_pass             unix:${config.services.phpfpm.pools.moodle.socket};
+        include ${config.services.nginx.package}/conf/fastcgi_params;
+        include ${config.services.nginx.package}/conf/fastcgi.conf;
       '';
     };
     locations."/dataroot/" = {
@@ -73,10 +79,11 @@
     post_max_size = 9000M
   '';
 
-  /*systemd.services.mysql.postStart = (lib.mkBefore ''
-    sleep 30s
-    export USER=root
-  '');*/
+  /* systemd.services.mysql.postStart = (lib.mkBefore ''
+       sleep 30s
+       export USER=root
+     '');
+  */
 
   # fix moodle bug: https://tracker.moodle.org/browse/MDL-72131
   services.mysql.settings.mysqld.innodb_read_only_compressed = false;
