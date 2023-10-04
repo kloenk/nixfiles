955c6c465d78ebbfda81051efb909448bf1200cd
Finn Behrens <me@kloenk.dev>
2023-10-04 15:57:36 +0200

    WIP: add emacs config

--- pkgs/emacs/doom.d/config.el
+++
@@ -1,7 +0,0 @@
-;;(setq doom-font (font-spec :family "Fira Mono" :size 12))
-(setq inhibit-splash-screen t)
-
-(after! rustic
-  (setq rustic-lsp-server 'rust-analyzer))
-
-(setq rustic-lsp-server 'rust-analyzer)
