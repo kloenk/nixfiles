88e56d05acf9e7211094bf31c1954d8b6aeff544
Finn Behrens <me@kloenk.de>
2023-11-12 18:44:43 +0100

    add proper values to varda

--- hosts/varda/links.nix
+++ hosts/varda/links.nix
@@ -7,12 +7,12 @@ let
 in {
   systemd.network = {
     links."10-eth0" = {
-      matchConfig.MACAddress = "96:00:02:ae:9b:77";
+      matchConfig.MACAddress = "96:00:02:b4:f1:e6";
       linkConfig.Name = "eth0";
     };
     networks."10-eth0" = {
       name = "eth0";
-      addresses = [{ addressConfig.Address = "2a01:4f8:c012:b874::/64"; }];
+      addresses = [{ addressConfig.Address = "2a01:4f8:c013:1a4b::/64"; }];
       routes = [{ routeConfig.Gateway = "fe80::1"; }];
       DHCP = "ipv4";
     };
