a01e8f0ae0359eac0dba04a230753dcdf13dd18c
Finn Behrens <me@kloenk.de>
2023-11-14 14:52:23 +0100

    bcachefs use 6.7-rc1

--- profiles/bcachefs.nix
+++ profiles/bcachefs.nix
@@ -2,24 +2,18 @@
   boot.initrd.supportedFilesystems = [ "vfat" "bcachefs" ];
   boot.supportedFilesystems = [ "bcachefs" "cifs" "vfat" "xfs" ];
 
-  # working:
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_6;
-  boot.kernelPatches =
-    let currentCommit = "b9bd69421f7364ca4ff11c827fd0e171a8b826ea";
-    in [{
-      name = "bcachefs-${currentCommit}";
-      patch = pkgs.fetchpatch {
-        name = "bcachefs-${currentCommit}.diff";
-        url =
-          "https://evilpiepirate.org/git/bcachefs.git/rawdiff/?id=${currentCommit}&id2=v6.6";
-        sha256 = "sha256-+Gp/1kTBgRx5a01l9xtaDbiLuOP58uZ7rx27WyYcMT4=";
-      };
-      extraConfig = ''
-        BCACHEFS_QUOTA y
-        BCACHEFS_POSIX_ACL y
-      '';
-      # extra config is inherited through boot.supportedFilesystems
-    }];
+  # 6.7-rc1:
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_testing;
+
+  boot.kernelPatches = [{
+    name = "bcachefs-config";
+    patch = null;
+    extraConfig = ''
+      BCACHEFS_QUOTA y
+      BCACHEFS_POSIX_ACL y
+    '';
+    # extra config is inherited through boot.supportedFilesystems
+  }];
 
   services.fstrim.enable = true;
 }
