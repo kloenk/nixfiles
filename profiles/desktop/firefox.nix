ca1d46e2eeb37a12f5fbb17c328ab246aed6ca2b
Finn Behrens <me@kloenk.de>
2023-11-17 12:53:00 +0100

    update flake and firefox profile

--- profiles/desktop/firefox.nix
+++ profiles/desktop/firefox.nix
@@ -12,10 +12,11 @@
           "https://addons.mozilla.org/firefox/downloads/latest/${shortName}/latest.xpi";
       }) {
         "FirefoxColor@mozilla.com" = "firefox-color";
-        "testpilot-containers@mozilla.com" = "multi-account-containers";
+        "@testpilot-containers" = "multi-account-containers";
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
         "uBlock0@raymondhill.net" = "ublock-origin";
-        "momentum@momentumdash.com" = "momentumdash";
+        # "momentum@momentumdash.com" = "momentumdash";
+        "{f7f203e0-9d1d-4557-891f-9865877c5b48}" = "tabby-cat-friend";
       };
       PasswordManagerEnabled = false;
       UserMessaging = {
