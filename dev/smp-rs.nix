e11cd1704a0b560e2bc2adde2d9f2649b5906278
Finn Behrens <me@kloenk.de>
2024-02-22 20:13:42 +0100

    dev/smp-rs: add darwin frameworks

--- dev/smp-rs.nix
+++ dev/smp-rs.nix
@@ -1,8 +1,11 @@
 { lib, stdenv, mkShell, rustc, cargo, rust-analyzer, nixfmt, rustfmt, pkg-config
-, udev, libiconv }:
+, udev, libiconv, darwin }:
 mkShell {
   nativeBuildInputs = [ rustc cargo rust-analyzer nixfmt rustfmt ]
     ++ lib.optional stdenv.isLinux [ pkg-config ];
-  buildInputs = (lib.optional stdenv.isDarwin libiconv)
-    ++ lib.optional stdenv.isLinux udev;
+  buildInputs = (lib.optionals stdenv.isDarwin [
+    libiconv
+    darwin.apple_sdk.frameworks.IOKit
+    darwin.apple_sdk.frameworks.CoreBluetooth
+  ]) ++ lib.optional stdenv.isLinux udev;
 }
