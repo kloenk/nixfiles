28c6ea25ec937e2f10fe76b1ab058866ece2053c
Fiona Behrens <me@kloenk.dev>
2025-03-24 13:57:43 +0100

    updat ekernel dev shell

--- dev/kernel.nix
+++ dev/kernel.nix
@@ -1,4 +1,4 @@
-{ mkShell, linux, pkg-config, ncurses, python3, rust-bin
+{ llvmPackages, mkShell, linux, pkg-config, ncurses, python3, b4, rust-bin
 , rust-version ? "1.81.0", rustAttrs ? rust-bin.stable.${rust-version}
 , rust-bindgen }:
 
@@ -12,6 +12,10 @@ mkShell {
 
     pkg-config
     ncurses
+
+    b4
+
+    llvmPackages.clang-tools
 
     (rustAttrs.default.override {
       extensions = [ "rust-src" "rust-analyzer" ];
