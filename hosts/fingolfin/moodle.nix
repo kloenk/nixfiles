543cb3b561b380f7a4b15f8d5e5cbd29bee1e502
Fiona Behrens <me@kloenk.dev>
2025-04-28 16:20:19 +0000

    fingolfin: change timeout for moodle

--- hosts/fingolfin/moodle.nix
+++ hosts/fingolfin/moodle.nix
@@ -64,6 +64,9 @@
         fastcgi_index            index.php;
         fastcgi_param   SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_pass             unix:${config.services.phpfpm.pools.moodle.socket};
+        fastcgi_connect_timeout 300s;
+        fastcgi_send_timeout 300s;
+        fastcgi_read_timeout 300s;
         include ${config.services.nginx.package}/conf/fastcgi_params;
         include ${config.services.nginx.package}/conf/fastcgi.conf;
       '';
