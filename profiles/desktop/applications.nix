7a039bf80302060b6a8734af80f54a657bff8b87
Fiona Behrens <me@kloenk.dev>
2025-05-20 14:49:48 +0000

    disable v4l2loopback as it taints the kernel

--- profiles/desktop/applications.nix
+++ profiles/desktop/applications.nix
@@ -35,11 +35,11 @@
     overskride
   ];
 
-  boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-  boot.kernelModules = [ "v4l2loopback" ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=2 exclusive_caps=1
-  '';
+  #boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+  #boot.kernelModules = [ "v4l2loopback" ];
+  #boot.extraModprobeConfig = ''
+  #  options v4l2loopback devices=2 exclusive_caps=1
+  #'';
 
   services.fwupd.enable = true;
 
